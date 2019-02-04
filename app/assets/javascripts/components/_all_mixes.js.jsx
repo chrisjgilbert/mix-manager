07a3118a8399c1cbe2bd0c6b4ee75b18b827e95f
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-04 18:43:23 +0000

    all mixes component added

--- app/assets/javascripts/components/_all_mixes.js.jsx
+++ app/assets/javascripts/components/_all_mixes.js.jsx
@@ -1,8 +1,30 @@
 class AllMixes extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mixes: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('api/v1/mixes.json')
+      .then((response) => { return response.json() })
+      .then((data) => { this.setState ({ mixes: data }) })
+  }
+
   render() {
+    let mixes = this.state.mixes.map((mix) => {
+      return (
+        <div key={mix.id}>
+          <h2>{mix.title}</h2>
+          <p>{mix.description}</p>
+          <a href={mix.url}>Link</a>
+        </div>
+      )
+    })
     return (
       <div>
-        <h1>Your Mix List:</h1>
+        {mixes}
       </div>
     )
   }

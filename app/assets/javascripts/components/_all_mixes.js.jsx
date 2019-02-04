9df402b36ddcd65875d055f41ea570c3b4fe2586
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-04 18:37:30 +0000

    seeded db

---
+++ app/assets/javascripts/components/_all_mixes.js.jsx
@@ -0,0 +1,9 @@
+class AllMixes extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Your Mix List:</h1>
+      </div>
+    )
+  }
+}
--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -1,7 +1,8 @@
 const Main = () => {
   return (
     <div>
-      <h1>Mix Manager</h1>
+    <h1>Mix Manager</h1>
+      <AllMixes />
     </div>
   )
 }
